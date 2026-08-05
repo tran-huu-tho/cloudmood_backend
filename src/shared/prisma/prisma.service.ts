8f5a73846099812ebe85c349c9401a62d9856109
Trần Hữu Thọ <hyunjuntho@gmail.com>
2026-08-06 03:58:18 +0700

    map

--- src/shared/prisma/prisma.service.ts
+++ src/shared/prisma/prisma.service.ts
@@ -9,7 +9,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      max: 10,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 10000,
+    });
     const adapter = new PrismaPg(pool);
     super({ adapter });
   }
